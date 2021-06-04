b9d3151c787024fd8cf37ace28e972fd1b311ca5
Bamby74 <dam.musset@gmail.com>
2021-06-04 17:47:33 +0200

    component Wrtiring et writingComment

--- frontend/groupomania-project/src/components/Writing.vue
+++ frontend/groupomania-project/src/components/Writing.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col-6 card mx-auto">
+    <main class="col-10 card mx-auto">
         <div class="card-header">
             <ul class="nav nav-tabs card-header-tabs">
                 <li class="nav-item">
@@ -14,39 +14,40 @@
         </div>
         <form id="post" class="card-body form" enctype="multipart/form-data">
             <div class="mb-3">
-                <textarea class="form-control" id="title" name="title" rows="3" placeholder="Title..." v-model="title"></textarea>
+                <span class=" form-control error alert-danger" v-if="validateForm">Vous ne pouvez poster une publication vide ...</span>
+                <br/>
+                <textarea class="form-control" id="title" name="title" rows="3" placeholder="Title..." v-model="title" @input="updateValidateForm"></textarea>
                 <br />
-                <textarea class="form-control" id="exampleFormControlTextarea1" name="content" rows="3" placeholder="Message..." v-model="content"></textarea>
-                <img id="image-preview" src="" class="img-fluid" alt="photo de commentaire" v-show="file" >
+                <textarea class="form-control" id="exampleFormControlTextarea1" name="content" rows="3" placeholder="Message..." v-model="content" @input="updateValidateForm"></textarea>
+                <img id="image-preview" src="" class="img-fluid" alt="aperç de la photo de publication" v-show="fileImage" >
+                <video v-show="fileVideo" width="100%" controls id="video-preview" src="" type="video/*" alt="aperçu de la video de publication"></video>
             </div>
-            <div class="row-link">
+            <div class="row-link justify-content-between">
                 <a href="#" class="btn btn-danger" @click.prevent="post">
                     <svg id="custom-picture" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cursor-fill" viewBox="0 0 16 16">
                         <path d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z"/>
                     </svg>
                     <p>Poster</p>
                 </a>
-                <input type="file" ref="file" id="image" name="file" accept="image/*" capture="user" @change="onFileSelected">
+                <input type="file" ref="file" id="image" name="file" accept="image/*,video/*" capture="user" @change="onFileSelected">
                 <div class="btn btn-danger" @click="addPicture">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="35" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
                         <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
                         <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
                     </svg>
                 </div>
-                <div class="btn btn-danger" id="close-picture" v-show="file" @click="file = ''">
+                <div class="btn btn-danger" id="close-picture" v-show="fileImage" @click="fileImage = ''">
                     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="35" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                         <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"/>
                     </svg>
                 </div>
             </div>
         </form>
-    </div>
+    </main>
 </template>
 
 <script>
 import axios from 'axios';
-import { mapActions } from 'vuex';
-import { token } from '../mixins/token';
 
 export default {
     name:"WritingPublications",
@@ -54,40 +55,44 @@ export default {
         return {
             title: "",
             content: "",
-            file:""
+            fileImage:"",
+            fileVideo: "",
+            validateForm: ""
         }
     },
     methods: {
-        ...mapActions(['addPublications']),
         post() {
-            axios.get('http://localhost:3000/api/auth/profil',{
-                headers: {
-                    'Authorization': 'Bearer ' + token
-                }
-            }).then(user => user.data.username)
-            .catch(error => console.log(error))
-            .then(username => {
+            if(!this.title && !this.content & !this.fileImage && !this.fileVideo) {
+                this.validateForm = true
+            }else{
                 const publication = {
-                    title: this.title,
-                    content: this.content,
+                title: this.title,
+                content: this.content,
                 }
-                console.log(username)
                 const formData = new FormData();
-                if(this.file) {
-                    formData.append('file', this.file)
+                if(this.fileImage) {
+                        formData.append('file', this.fileImage)
+                    }
+                if(this.fileVideo) {
+                    formData.append('file', this.fileVideo)
                 }
                 formData.append('publication', JSON.stringify(publication))
+                let token = localStorage.getItem('token')
                 axios.post('http://localhost:3000/api/publications', formData, {
                     headers: {
                     'Authorization' : 'Bearer '+token
                     }   
-                }).then(() => {
+                })
+                .then(() => {
                     this.$router.push({ name: "publications" })
                 })
+                .catch(error => console.log(error))
                 this.title = "",
                 this.content = "",
-                this.file= ""
-            }).catch(error => console.log(error))      
+                this.fileImage = "" ,
+                this.fileVideo = "",
+                this.validateForm = false  
+            }
         },
         addPicture() {
             const realFileBtn = document.getElementById("image");
@@ -96,17 +101,30 @@ export default {
         onFileSelected(event) {
             const imageToPost = event.target.files
             if(imageToPost === 0) {
-                console.log("Aucune image sélectionnée !")
+            console.log("Aucune image sélectionnée !")
             }else{
-                let previewImage = document.getElementById('image-preview')
-                this.file = imageToPost[0]
-                const reader = new FileReader()
-                reader.addEventListener("load", function() {
-                    previewImage.setAttribute("src", this.result)
-                })
-                reader.readAsDataURL(this.file)
-                // reader.readAsText(input.files[0])
+                if(imageToPost[0].type === ("image/jpeg" || "image/png" || "image/jpg" || "image/.gif")) {
+                    let previewImage = document.getElementById('image-preview')
+                    this.fileImage = imageToPost[0]
+                    const reader = new FileReader()
+                    reader.addEventListener("load", function() {
+                        previewImage.setAttribute("src", this.result)
+                    })
+                    reader.readAsDataURL(this.fileImage)
+                }else{
+                    let previewVideo = document.getElementById('video-preview')
+                    this.fileVideo = imageToPost[0]
+                    const reader = new FileReader()
+                    reader.addEventListener("load", function() {
+                        previewVideo.setAttribute("src", this.result)
+                    })
+                    reader.readAsDataURL(this.fileVideo)
+                }
+                this.updateValidateForm()
             }
+        },
+        updateValidateForm() {
+            return this.validateForm = ""
         }
     }
 }
@@ -158,8 +176,12 @@ ul svg, span svg {
 #close-picture {
     margin-left: 10px;
 }
-img {
+img,video {
     margin-top: 10px;
     border-radius: 10px;
+}
+.alert-danger {
+    border-radius: 3px;
+    padding: 5px;
 }
 </style>
--- frontend/groupomania-project/src/components/WritingComment.vue
+++ frontend/groupomania-project/src/components/WritingComment.vue
@@ -1,46 +1,60 @@
 <template>
-    <div class="col-10 mx-auto">
-        <textarea class="form-control" id="content-comment" v-model="content" name="comment" placeholder="Que voulez-vous dire ?"></textarea>
-        <button class="btn btn-danger" @click.prevent="postComment">Commenter</button>
-    </div>
+    <section class="row">
+        <article class="col-10 mx-auto">
+            <div class="alert-danger">
+                <p v-if="(!$v.content.required && $v.content.dirty) || this.submitStatus == 'ERROR'">Vous ne pouvez faire un commentaire sans écrire au minimum une lettre !</p>
+            </div>
+            <div v-if="(!$v.content.maxLength && this.submitStatus == 'ERROR')" class="alert-danger">
+                <p>Vous avez atteint le nombre maximales de 250 caractères</p>
+            </div>
+            <textarea class="form-control" id="content-comment" v-model="$v.content.$model" name="comment" placeholder="Que voulez-vous dire ?" required @input="changeSubmitStatus"></textarea>
+            <button data-message=" bouton pour valider le comentaire" class="btn btn-danger" @click.prevent="submit">Commenter</button>
+        </article>
+    </section>
 </template>
 
 <script>
-// import axios from 'axios';
-// import { token } from '../mixins/token';
 import {mapActions} from 'vuex';
+import { required, maxLength } from 'vuelidate/lib/validators';
 
 export default {
     name:  "WritingComment",
     data() {
         return {
-            content: ""
+            content: "",
+            submitStatus: null
+        }
+    },
+    validations: {
+        content: {
+            required,
+            maxLength: maxLength(250)
         }
     },
     methods: {
         ...mapActions(['createComment']),
-        postComment(){
-            let publicationId = localStorage.getItem('publicationId')
-            const newComment = {
-                content: this.content,
-                publicationId: publicationId
+        submit() {
+            this.$v.$touch()
+            if(this.$v.$invalid) {
+                this.submitStatus = 'ERROR'
+            } else {
+                let publicationId = localStorage.getItem('publicationId')
+                const newComment = {
+                    content: this.content,
+                    publicationId: publicationId
+                }
+                this.createComment(newComment)
+                this.submitStatus = 'PENDING'
+                setTimeout(() => {
+                    this.submitStatus = 'OK'
+                },250)
+                window.scroll(0,0)
+                this.content = "";
             }
-            this.createComment(newComment)
-            this.content = "";
+        },
+        changeSubmitStatus() {
+            return this.submitStatus = null
         }
-        // createComment() {
-        //     const comment = {
-        //         content: this.content,
-        //         publicationId: localStorage.getItem('publicationId')
-        //     }
-        //     axios.post('http://localhost:3000/api/comments', comment, {
-        //          headers: {
-        //             'Authorization': 'Bearer ' + token
-        //         }
-        //     })
-        //     .then(() => this.$router.push({ name: 'comments' }))
-        //     .catch(error => console.log(error))
-        // }
     }
 }
 </script>
@@ -56,12 +70,16 @@ export default {
 textarea {
     height: 150px;
     border-radius: 10px;
-    border-color: rgba(175, 174, 174, 0.282);
+    border-color: rgba(250, 24, 24, 0.282);
     margin-bottom: 20px;
 }
 textarea:focus {
     outline: 0 none;
     border-color: rgb(147, 17, 43, 0.282);
     box-shadow: 0 0 0 0.2rem rgba(147, 17, 43, 0.282)
+}
+.alert-danger {
+    border-radius: 10px;
+    margin: 10px 0px;
 }
 </style>
