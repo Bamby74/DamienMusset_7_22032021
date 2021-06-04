d524c1fe4c91fdcecdbb4a811d48ac1cb04701fa
Bamby74 <dam.musset@gmail.com>
2021-06-04 17:43:34 +0200

    frontent component comment

--- frontend/groupomania-project/src/components/Comment.vue
+++ frontend/groupomania-project/src/components/Comment.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="col-10 mx-auto ">
         <div class="col card card-body">
-            <h6>{{ comment.username}}</h6>
+            <div class="d-flex justify-content-between">
+                <h6>{{ comment.username}}</h6>
+                <svg v-if="userAdmin" id="rubish" @click="deletedComment" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                </svg>
+            </div>
             <p class="card-text text-light">{{ comment.content }}</p>
         </div>
         <p class="col-6">{{ setDate(comment.updatedAt) }}</p>
@@ -10,10 +15,31 @@
 
 <script>
 import { setDate } from '../mixins/date';
+import { mapActions } from 'vuex';
+
 export default {
     name: 'Comment',
     props: ['comment'],
-    mixins: [setDate]
+    mixins: [setDate],
+    data() {
+        return {
+            userAdmin: false
+        }
+    },
+    methods: {
+        ...mapActions(['deleteComment']),
+        deletedComment() {
+            this.deleteComment(this.comment)
+        },
+        adminLow() {
+            if(localStorage.getItem('admin') === "true") {
+                this.userAdmin = true
+            }
+        },
+    },
+    created() {
+        this.adminLow()
+    }
 }
 </script>
 
@@ -43,5 +69,11 @@ p.col-6 {
     font-style: italic;
     text-align: left;
     margin-bottom: 5px;
+}
+#rubish {
+    margin-left: 10px;
+    color: white;
+    cursor: pointer;
+    margin-right: 0px;
 }
 </style>
