55d744cb7276b00c566d13709fef448f2f470f60
Bamby74 <dam.musset@gmail.com>
2021-06-04 17:54:18 +0200

    models user

--- backend/models/user.js
+++ backend/models/user.js
@@ -25,14 +25,20 @@ const User = db.define('user', {
   email: {
     type: DataTypes.STRING,
     allowNull: false,
+    unique: true
   },
   password: {
     type: DataTypes.STRING,
+    allowNull: false
   },
   username: {
     type: DataTypes.STRING,
     allowNull: false
   },
+  isAdmin: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false
+  }
 })
 
 module.exports = User ;
