b8826f1fe3a104951dff360df1bea50985227b71
Bamby74 <dam.musset@gmail.com>
2021-06-04 17:49:50 +0200

    mixins

--- frontend/groupomania-project/src/mixins/date.js
+++ frontend/groupomania-project/src/mixins/date.js
@@ -6,10 +6,15 @@ export const setDate = {
             const publicationMonth = publicationDate.getMonth();
             const publicationYear = publicationDate.getFullYear();
             const publicationHours = publicationDate.getHours();
-            const publicationMinutes = publicationDate.getMinutes();
+            let publicationMinutes = publicationDate.getMinutes();
             
             let tab_month= ["Janvier","Février","Mars","Avril","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"]
-            return `Le ${publicationDay} ${tab_month[publicationMonth+2]} ${publicationYear}, à ${publicationHours}h${publicationMinutes}`
+            if(publicationMinutes < 10){
+                publicationMinutes = `0`+publicationMinutes
+            }
+            let configDate = publicationDay+' '+tab_month[publicationMonth+2]+' '+ publicationYear
+            // ${publicationDay} ${tab_month[publicationMonth+2]} ${publicationYear}
+            return `Le ${configDate}, à ${publicationHours}h${publicationMinutes}`
         }
     }
 }
--- frontend/groupomania-project/src/mixins/token.js
+++ frontend/groupomania-project/src/mixins/token.js
@@ -1,2 +1,3 @@
 export const token = localStorage.getItem('token')
 
+
